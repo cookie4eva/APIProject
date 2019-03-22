bed58ecdde6a5abf72ddb900e9fe986898196ef2
cookie4eva <hila.fox@gmail.com>
2019-03-22 14:05:55 +0200

    remove imports

--- my-greeting-api/src/main/java/greeting/controller/GreetingCardsController.java
+++ my-greeting-api/src/main/java/greeting/controller/GreetingCardsController.java
@@ -1,9 +1,7 @@
 package greeting.controller;
 
-import api.dto.AllGCTemplatesDTO;
 import lombok.Data;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
